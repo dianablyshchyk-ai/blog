1eb8687281d044358aa342fa9be19e31832d4116
Diana <diana.blyshchyk@oa.edu.ua>
2026-05-25 11:25:20 +0300

    feat: complete lab4

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+//use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //dd(__METHOD__);
+        $paginator = BlogCategory::paginate(5);
+
+        return $paginator;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $item = new BlogCategory($data);
+
+        $result = $item->save();
+
+        if ($result) {
+            return ['success' => 'Успішно збережено'];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $item = BlogCategory::find($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return back() //redirect back
+            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
+            ->withInput(); //повернути дані
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return ['success' => 'Успішно збережено'];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
